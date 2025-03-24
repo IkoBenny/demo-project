9980f13268d9a318cc1db76f8847e670a9839ae4
IkoBenny <benny.iko.it@gmail.com>
2025-03-23 21:15:18 -0400

    On branch master Changes to be committed: 	modified:  DemoProjectApplication.java
    mapping HTTP requests onto specific handler messages
    
    query parameter example: users?id=4&name=joe
    
    @RestController
    @RequestMapping("/")
    @GetMapping("get/")
    @PostMapping("save/")
    @PutMappping("edit/")
    @DeleteMapping("remove/")

--- demo-project/src/main/java/com/examples/demo/demo_project/DemoProjectApplication.java
+++ demo-project/src/main/java/com/examples/demo/demo_project/DemoProjectApplication.java
@@ -2,10 +2,24 @@ package com.examples.demo.demo_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoProjectApplication {
 
+    @GetMapping("/hello-world")
+    public String getCasualWelcomeMessage() {
+        return "Hey World...!!!";
+    }
+    
+    @GetMapping("/")
+    public String getGenericWelcomeMessage() {
+        return "Hello World...!!!";
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
